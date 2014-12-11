203a553040b032bd6e22a4a283cbd35b97230292
Teodor <Teodor@Teodor-Komputer>
2014-12-11 15:53:11 +0100

    dao cd

---
+++ ZKimNaBasen/src/Dziecioly/zkimnabasen/baza/dao/WydarzenieDao.java
@@ -0,0 +1,8 @@
+package Dziecioly.zkimnabasen.baza.dao;
+
+import Dziecioly.zkimnabasen.baza.model.Wydarzenie;
+
+public class WydarzenieDao extends GenericDao<Wydarzenie, Integer>{
+
+	
+}
---
+++ ZKimNaBasen/src/Dziecioly/zkimnabasen/baza/dao/ZaproszenieDao.java
@@ -0,0 +1,7 @@
+package Dziecioly.zkimnabasen.baza.dao;
+
+import Dziecioly.zkimnabasen.baza.model.Zaproszenie;
+
+public class ZaproszenieDao extends GenericDao<Zaproszenie, Integer>{
+
+}
