2a677b138c272a7ff17f35916094184f5e620de1
Teodor <Teodor@Teodor-Komputer>
2014-12-22 05:49:46 +0100

    dialog czy usunac wyd?

--- ZKimNaBasen/src/Dziecioly/zkimnabasen/activity/SzczegolyWydarzenia.java
+++ ZKimNaBasen/src/Dziecioly/zkimnabasen/activity/SzczegolyWydarzenia.java
@@ -9,10 +9,12 @@ import Dziecioly.zkimnabasen.baza.model.List_Custom_ListaWydarzen;
 import Dziecioly.zkimnabasen.baza.model.RowBeanListaWyd;
 import Dziecioly.zkimnabasen.baza.model.Wydarzenie;
 import Dziecioly.zkimnabasen.baza.model.Zaproszenie;
+import Dziecioly.zkimnabasen.fragment.YesNoFragment;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -23,16 +25,16 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class SzczegolyWydarzenia extends Activity {
+public class SzczegolyWydarzenia extends FragmentActivity {
 
 	static final String DEBUG_TAG = "LOG";
-	
+
 	private TextView tv_tworca;
 	private TextView tv_nazwa;
 	private TextView tv_data;
 	private TextView tv_od;
 	private TextView tv_do;
-	private TextView tv_opis; 
+	private TextView tv_opis;
 	private ListView rozbudowana_lista;
 	private Button btnEdytuj;
 	private Button btnUsun;
@@ -47,7 +49,7 @@ public class SzczegolyWydarzenia extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.szczegoly_wydarzenia);
 		context = getApplicationContext();
-		
+
 		Intent intent = getIntent();
 		id = intent.getIntExtra("id_wydarzenia", -1);
 
@@ -66,41 +68,52 @@ public class SzczegolyWydarzenia extends Activity {
 
 		czytajWydarzenie(id);
 	}
-	
+
 	private void czytajWydarzenie(int id) {
-		
-		Wydarzenie wydarzenie = wydDao.find(id); // pobierz wydarzenie, select do bazy
-		
-		Log.d(DEBUG_TAG, "czytajWydarzenie2 id="+id);
-		
-		if (wydarzenie.equals(null))	// czy wydarzenie sie pobralo
+
+		Wydarzenie wydarzenie = wydDao.find(id); // pobierz wydarzenie, select
+													// do bazy
+
+		Log.d(DEBUG_TAG, "czytajWydarzenie2 id=" + id);
+
+		if (wydarzenie.equals(null)) // czy wydarzenie sie pobralo
 			Log.d(DatabaseManager.DEBUG_TAG, "Brak wydarzenia");
 		else { // jesli tak to dzialaj
-			tv_tworca.setText(wydarzenie.getUzytkownik().getNazwa());	// wprowadzenie do wszystkich TextView info o wydarzeniu
+			tv_tworca.setText(wydarzenie.getUzytkownik().getNazwa()); // wprowadzenie
+																		// do
+																		// wszystkich
+																		// TextView
+																		// info
+																		// o
+																		// wydarzeniu
 			tv_nazwa.setText(wydarzenie.getNazwa());
 			tv_data.setText(General.stringFromDate(wydarzenie.getData()));
 			tv_od.setText(wydarzenie.getGodz_od());
 			tv_do.setText(wydarzenie.getGodz_do());
 			tv_opis.setText(wydarzenie.getOpis());
-			
+
 			List<Zaproszenie> zaproszeniaL = wydarzenie.getZaproszenia();
-			Log.d(DEBUG_TAG, "Iloœc zaproszeñ: "+ zaproszeniaL.size());
-			
-			
-			if(zaproszeniaL.isEmpty())
-				Toast.makeText(this, "Lista zaproszeñ jest pusta w ciul!", Toast.LENGTH_LONG).show();
+			Log.d(DEBUG_TAG, "Iloœc zaproszeñ: " + zaproszeniaL.size());
+
+			if (zaproszeniaL.isEmpty())
+				Toast.makeText(this, "Lista zaproszeñ jest pusta w ciul!",
+						Toast.LENGTH_LONG).show();
 
 			else {
-				RowBeanListaWyd[] ZaproszeniaRB = new RowBeanListaWyd[zaproszeniaL.size()];
+				RowBeanListaWyd[] ZaproszeniaRB = new RowBeanListaWyd[zaproszeniaL
+						.size()];
 
-				for (int i=0; i < ZaproszeniaRB.length; i++) {
+				for (int i = 0; i < ZaproszeniaRB.length; i++) {
 					ZaproszeniaRB[i] = new RowBeanListaWyd();
-					ZaproszeniaRB[i].setTekst(zaproszeniaL.get(i).getUzytkownik().getNazwa());
+					ZaproszeniaRB[i].setTekst(zaproszeniaL.get(i)
+							.getUzytkownik().getNazwa());
 					ZaproszeniaRB[i].setIcon(R.drawable.wiewiorka);
 				}
 
 				rozbudowana_lista = (ListView) findViewById(R.id.lv_prostalista);
-				List_Custom_ListaWydarzen adapter_listy = new List_Custom_ListaWydarzen(this, R.layout.view_row_item_lista_wydarzen, ZaproszeniaRB);
+				List_Custom_ListaWydarzen adapter_listy = new List_Custom_ListaWydarzen(
+						this, R.layout.view_row_item_lista_wydarzen,
+						ZaproszeniaRB);
 				rozbudowana_lista.setAdapter(adapter_listy);
 			}
 		}
@@ -119,11 +132,8 @@ public class SzczegolyWydarzenia extends Activity {
 		btnUsun.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Wydarzenie w = wydDao.find(id);
-				wydDao.remove(w);
-				Toast.makeText(context, "Usuniêto", Toast.LENGTH_LONG).show();
-				Intent intent = new Intent(context, WydarzeniaLista.class);
-				startActivity(intent);
+				YesNoFragment frag = new YesNoFragment(id);
+				frag.show(getSupportFragmentManager(), "YesNo");
 
 			}
 		});
@@ -143,13 +153,10 @@ public class SzczegolyWydarzenia extends Activity {
 		if (id == R.id.action_logout) {
 			General.clearSharedPrefs(context);
 			return true;
-		}
-		else if(id == R.id.action_clear) {
-			General.clearData(context);	
+		} else if (id == R.id.action_clear) {
+			General.clearData(context);
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
 }
-
-
